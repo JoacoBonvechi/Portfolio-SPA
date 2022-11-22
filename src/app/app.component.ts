d3933ae2b4e1b9aa1ed5367a308f1427ca5e5a14
JoacoBonvechi <joacobonvechi2272002@gmail.com>
2022-11-22 12:50:39 -0300

    empezando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+  
 }
+
+
+const maquinaEscribir1 = (text1 = '', tiempo1 = 200, etiqueta1: { innerHTML: string; } ) => {
+    let arrayCaracteres1 = text1.split('')
+    etiqueta1.innerHTML = ''
+    let cont1 = 0
+    let escribir1 = setInterval(function(){
+        etiqueta1.innerHTML += arrayCaracteres1[cont1]
+        cont1++
+        if(cont1 === arrayCaracteres1.length){
+            clearInterval(escribir1)
+        }
+
+    },tiempo1)
+    
+};
+
+
