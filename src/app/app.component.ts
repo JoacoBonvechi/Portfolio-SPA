8078f5f449079f2e580c4b27e79ec1ec92355632
JoacoBonvechi <joacobonvechi2272002@gmail.com>
2022-11-22 18:12:19 -0300

    idea NO responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,19 +11,3 @@ export class AppComponent {
 }
 
 
-const maquinaEscribir1 = (text1 = '', tiempo1 = 200, etiqueta1: { innerHTML: string; } ) => {
-    let arrayCaracteres1 = text1.split('')
-    etiqueta1.innerHTML = ''
-    let cont1 = 0
-    let escribir1 = setInterval(function(){
-        etiqueta1.innerHTML += arrayCaracteres1[cont1]
-        cont1++
-        if(cont1 === arrayCaracteres1.length){
-            clearInterval(escribir1)
-        }
-
-    },tiempo1)
-    
-};
-
-
