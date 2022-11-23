2e2da4354084126d566e671ff52e30ad22186e5d
JoacoBonvechi <joacobonvechi2272002@gmail.com>
2022-11-22 22:18:47 -0300

    mati puto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  texto :string= ""
+  maquina2 = document.getElementById('presentacion-text')
+  maquina3 = document.getElementById('presentacion-text2')
+
+ 
   
 }
-
 
