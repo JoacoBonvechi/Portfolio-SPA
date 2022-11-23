8aceceef351be743042b1fe51efc8a9da59a2b96
JoacoBonvechi <joacobonvechi2272002@gmail.com>
2022-11-22 22:24:46 -0300

    rama rota

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,22 @@ export class AppComponent {
   maquina2 = document.getElementById('presentacion-text')
   maquina3 = document.getElementById('presentacion-text2')
 
- 
+  public maquinaEscribir1(text1 = '', tiempo1 = 200) {
+    let arrayCaracteres1 = text1.split('')
+    this.texto = ''
+    let cont1 = 0
+    let escribir1 = setInterval(function(){
+        texto += arrayCaracteres1[cont1]
+        cont1++
+        if(cont1 === arrayCaracteres1.length){
+            clearInterval(escribir1)
+        }
+
+    },tiempo1)
+  }
+
+  ngOnInit(): void {
+    this.maquinaEscribir1("Hello, I'm Joaquin",100)
+  }
   
 }
-
