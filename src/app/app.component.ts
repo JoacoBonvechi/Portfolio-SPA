c198ca0965a2c8ae79477c833e51be80fc6313d8
JoacoBonvechi <joacobonvechi2272002@gmail.com>
2022-11-22 23:54:19 -0300

    maquina casi funcional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,16 @@ import { Component, OnInit} from '@angular/core';
 export class AppComponent {
   title = 'portfolio';
 
-  texto :string= ""
-  maquina2 = document.getElementById('presentacion-text')
-  maquina3 = document.getElementById('presentacion-text2')
+  texto = ""
+  texto2 = ""
+  texto3 = ""
 
   public maquinaEscribir1(text1 = '', tiempo1 = 200) {
     let arrayCaracteres1 = text1.split('')
-    this.texto = ''
+    let texto = ""
     let cont1 = 0
-    let escribir1 = setInterval(function(){
-        texto += arrayCaracteres1[cont1]
+    let escribir1 = setInterval(()=>{
+        this.texto += arrayCaracteres1[cont1]
         cont1++
         if(cont1 === arrayCaracteres1.length){
             clearInterval(escribir1)
@@ -26,8 +26,19 @@ export class AppComponent {
     },tiempo1)
   }
 
-  ngOnInit(): void {
+  public escritura(){
     this.maquinaEscribir1("Hello, I'm Joaquin",100)
+    setTimeout(() => {
+      this.maquinaEscribir1("Full Stack Developer", 70)
+  },2000);
+  setTimeout(() => {
+    this.maquinaEscribir1("Front End Specialist.", 70)
+},3500);
   }
+
+  ngOnInit(): void {
+    this.escritura()
+  }
+
   
 }
