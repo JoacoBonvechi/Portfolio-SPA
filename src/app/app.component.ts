19fbbcf0dfaf9d91a7d81af09054c8cbbdf3b538
JoacoBonvechi <joacobonvechi2272002@gmail.com>
2022-11-23 13:26:29 -0300

    componentes agregados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,38 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'portfolio';
 
-  texto = ""
-  texto2 = ""
-  texto3 = ""
+  texto = '';
+  texto2 = '';
+  texto3 = '';
 
-  public maquinaEscribir1(text1 = '', tiempo1 = 200) {
-    let arrayCaracteres1 = text1.split('')
-    let texto = ""
-    let cont1 = 0
-    let escribir1 = setInterval(()=>{
-        this.texto += arrayCaracteres1[cont1]
-        cont1++
-        if(cont1 === arrayCaracteres1.length){
-            clearInterval(escribir1)
-        }
-
-    },tiempo1)
-  }
-
-  public escritura(){
-    this.maquinaEscribir1("Hello, I'm Joaquin",100)
-    setTimeout(() => {
-      this.maquinaEscribir1("Full Stack Developer", 70)
-  },2000);
-  setTimeout(() => {
-    this.maquinaEscribir1("Front End Specialist.", 70)
-},3500);
+  public maquinaEscribir1(text1 = '', tiempo1 = 200, variable: string) {
+    let arrayCaracteres1 = text1.split('');
+    let cont1 = 0;
+    let escribir1 = setInterval(() => {
+      variable += arrayCaracteres1[cont1];
+      cont1++;
+      if (cont1 === arrayCaracteres1.length) {
+        clearInterval(escribir1);
+      }
+    }, tiempo1);
   }
 
   ngOnInit(): void {
-    this.escritura()
-  }
+    this.maquinaEscribir1("Hello, I'm Joaquin", 100, this.texto)
 
-  
+    setTimeout(() => {
+      this.maquinaEscribir1('Full Stack Developer', 70, this.texto2)
+    }, 2000)
+    
+    setTimeout(() => {
+      this.maquinaEscribir1('Front End Specialist.', 70, this.texto3)
+    }, 3500)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
+import { LanguagesComponent } from './component/languages/languages.component';
+import { AboutComponent } from './component/about/about.component';
+import { WorkComponent } from './component/work/work.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LanguagesComponent,
+    AboutComponent,
+    WorkComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
