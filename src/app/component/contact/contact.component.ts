5281265b706e37022be9f208431594c58360f532
Joaquin Bonvechi <joacobonvechi2272002@gmail.com>
2023-03-28 20:35:09 -0300

    envio de formulario funcionando

--- src/app/component/contact/contact.component.ts
+++ src/app/component/contact/contact.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
 
+export class ContactComponent {
+  @ViewChild('myForm')
+  myForm!: { nativeElement: any; };
+
+  onSubmit() {
+    // Obtener el elemento DOM del formulario
+    const form = this.myForm.nativeElement;
+    
+    // Enviar el formulario
+    form.submit();
+  }
+  
 }
